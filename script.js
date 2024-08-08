9e404d83666244185531ff2ed3450ffa91c603f3
Luc4sac <110491837+Luc4sac@users.noreply.github.com>
2024-08-08 08:34:32 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const caixaPrincipal=document.querySelector('caixa-principal');
+const caixaPergunta=document.querySelector('caixa-pergunta');
+const caixaResultado=document.querySelector('caixa-resultado');
+const caixaAlternativa=document.querySelector('caixa-alternativa');
+
+const pergunta=[
+    {
+        enunciado:"Pergunta 1",
+        Alternativas:[ "Alternativa 1", "Alternativa 2" ]
+    },
+{
+    enunciado:"Pergunta 2",
+    Alternativas:[ "Alternativa 1", "Alternativa 2" ]
+},
+{
+    enunciado:"Pergunta 3",
+    Alternativas:[ "Alternativa 1", "Alternativa 2" ]
+},
+]
+let posicao = 0;
+let perguntaAtual;
+
+function mostraPergunta(){
+
+}
