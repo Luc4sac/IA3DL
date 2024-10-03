40234d073fb64c61e5b4b609a719989e502a25cf
Luc4sac <110491837+Luc4sac@users.noreply.github.com>
2024-10-03 08:37:15 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,15 +6,43 @@ const caixaResultado = document.querySelector('.caixa-resultado');
 const perguntas = [
     {
         enunciado: "Pergunta 1",
-        alternativas:["Alternativa 1","Alternativa 2"]
+        alternativas:[
+            {
+                texto: "Sim",
+                afirmativa:"Afirmativa da alternativa 1"
+            }
+            {
+                texto: "Não",
+                afirmativa:"Afirmativa da alternativa 1"
+            }
+        ]
+
     },
     {
         enunciado: "Pergunta 2",
-        alternativas:["Alternativa 1","Alternativa 2"]
+        alternativas:[
+            {
+                texto: "Sim",
+                afirmativa:"Afirmativa da alternativa 2"
+            }
+            {
+                texto: "Não",
+                afirmativa:"Afirmativa da alternativa 2"
+            }
+        ]
     },
     {
         enunciado: "Pergunta 3",
-        alternativas:["Alternativa 1","Alternativa 2"]
+        alternativas:[
+            {
+                texto: "Sim",
+                afirmativa:"Afirmativa da alternativa 3"
+            }
+            {
+                texto: "Não",
+                afirmativa:"Afirmativa da alternativa 3"
+            }
+        ]
     }
 ]
 
